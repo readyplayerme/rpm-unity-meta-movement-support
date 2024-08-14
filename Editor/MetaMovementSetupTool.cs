45cbb15d028232e70ee7eb0cde1b197ae9dc6857
Harrison <harrisonhough4@gmail.com>
2024-08-14 13:34:39 +0300

    fix: tentative fix for custom face error

--- Editor/MetaMovementSetupTool.cs
+++ Editor/MetaMovementSetupTool.cs
@@ -78,7 +78,6 @@ namespace ReadyPlayerMe.MetaMovement.Editor
                 {
                     arkitFaceComponent = mesh.gameObject.AddComponent<ARKitFace>();
                 }
-                arkitFaceComponent.OnBeforeSerialize();
                 MetaMovementHelper.ApplyARKitFaceSettings(arkitFaceComponent);
                 EditorUtility.SetDirty(arkitFaceComponent);
             }
